b43bb7ac28224ab1974654624ba064583ee79fd1
cgugas <spartan.1086@gmail.com>
2017-04-30 19:45:25 -0500

    added KSDK and build scripts

---
+++ software/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/CMakeFiles/KsdkPlatformLib.dir/home/hk47/My_media/nifty_software/git/ahabadge/KSDK_1.2.0/platform/drivers/src/gpio/fsl_gpio_driver.c.d
@@ -0,0 +1,68 @@
+CMakeFiles/KsdkPlatformLib.dir/home/hk47/My_media/nifty_software/git/ahabadge/KSDK_1.2.0/platform/drivers/src/gpio/fsl_gpio_driver.c.obj: \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/platform/drivers/src/gpio/fsl_gpio_driver.c \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/drivers/inc/fsl_gpio_driver.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_port_hal.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/fsl_device_registers.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cm0plus.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmInstr.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmFunc.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/startup/system_MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_extension.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/fsl_bitaccess.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_features.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_gpio_hal.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/system/inc/fsl_clock_manager.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_sim_hal.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/../src/sim/MKL27Z4/fsl_sim_hal_MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/system/inc/../src/clock/MKL27Z4/fsl_clock_MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_mcglite_hal_modes.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_osc_hal.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/system/inc/fsl_interrupt_manager.h
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/drivers/inc/fsl_gpio_driver.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_port_hal.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/fsl_device_registers.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cm0plus.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmInstr.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmFunc.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/startup/system_MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_extension.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/fsl_bitaccess.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_features.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_gpio_hal.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/system/inc/fsl_clock_manager.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_sim_hal.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/../src/sim/MKL27Z4/fsl_sim_hal_MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/system/inc/../src/clock/MKL27Z4/fsl_clock_MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_mcglite_hal_modes.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_osc_hal.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/system/inc/fsl_interrupt_manager.h:
---
+++ software/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/CMakeFiles/KsdkPlatformLib.dir/home/hk47/My_media/nifty_software/git/ahabadge/KSDK_1.2.0/platform/hal/src/smc/fsl_smc_hal.c.d
@@ -0,0 +1,35 @@
+CMakeFiles/KsdkPlatformLib.dir/home/hk47/My_media/nifty_software/git/ahabadge/KSDK_1.2.0/platform/hal/src/smc/fsl_smc_hal.c.obj: \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/platform/hal/src/smc/fsl_smc_hal.c \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_smc_hal.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/fsl_device_registers.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cm0plus.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmInstr.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmFunc.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/startup/system_MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_extension.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/fsl_bitaccess.h \
+ /home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_features.h
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/hal/inc/fsl_smc_hal.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/fsl_device_registers.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cm0plus.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmInstr.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/CMSIS/Include/core_cmFunc.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/startup/system_MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_extension.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/fsl_bitaccess.h:
+
+/home/hk47/My\ media/nifty\ software/git/ahabadge/KSDK_1.2.0/lib/ksdk_platform_lib/armgcc/KL27Z4/../../../../platform/devices/MKL27Z4/include/MKL27Z4_features.h:
