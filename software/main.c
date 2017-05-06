fdcf247ff216b2fead3d4b52c7160409656797b6
cgugas <spartan.1086@gmail.com>
2017-05-06 18:18:21 -0500

    test code

---
+++ software/main.c
@@ -0,0 +1,32 @@
+ // Standard C Included Files
+#include <stdio.h>
+// SDK Included Files
+#include "fsl_os_abstraction.h"
+#include "fsl_i2c_hal.h"
+#include "fsl_i2c_master_driver.h"
+#include "fsl_clock_manager.h"
+#include "fsl_debug_console.h"
+
+#define DATA_LENGTH             64
+#define BOARD_I2C_INSTANCE          1
+
+// Buffer store data to send to slave
+uint8_t txBuff[DATA_LENGTH] = {0};
+// Buffer store data to receive from slave
+uint8_t rxBuff[DATA_LENGTH] = {0};
+
+i2c_device_t compass=
+{
+	.address=0x32,
+	.baudRate_kbps=400
+};
+
+int main (void)
+{
+	i2c_
+		//http://www.nxp.com/assets/documents/data/en/supporting-information/Inter-Integrated-Circuit-Training.pdf
+
+    return 0;
+}
+
+
