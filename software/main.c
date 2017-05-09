45c6764955de6256caeb8cbcf928e90087f19a58
cgugas <spartan.1086@gmail.com>
2017-05-08 20:59:18 -0500

    compass test code, halts after send

--- software/main.c
+++ software/main.c
@@ -8,7 +8,7 @@
 #include "fsl_debug_console.h"
 
 #define DATA_LENGTH             64
-#define BOARD_I2C_INSTANCE          1
+#define BOARD_I2C_INSTANCE      1
 
 // Buffer store data to send to slave
 uint8_t txBuff[DATA_LENGTH] = {0};
@@ -21,12 +21,44 @@ i2c_device_t compass=
 	.baudRate_kbps=400
 };
 
+i2c_master_state_t compass_state;
+
+
 int main (void)
 {
-//	i2c_
-		//http://www.nxp.com/assets/documents/data/en/supporting-information/Inter-Integrated-Circuit-Training.pdf
+    // Initialize i2c master
+    I2C_DRV_MasterInit(BOARD_I2C_INSTANCE, &compass_state);
 
+	// Master sends 1 bytes CMD and data to slave
+    uint32_t cmd=0x50;
+
+	// hardware_init();
+
+    I2C_DRV_MasterSendDataBlocking(BOARD_I2C_INSTANCE, &compass, (const uint8_t*)&cmd, 1, (const uint8_t*)txBuff, cmd, 1000);
+
+    int red=5;
+    red =6;
+    // Delay to wait slave received data
+    // OSA_TimeDelay(25);
+
+    // Master receives count byte data from slave
+    I2C_DRV_MasterReceiveDataBlocking(BOARD_I2C_INSTANCE, &compass, NULL, 0, rxBuff, cmd, 1000);
+
+    rxBuff;
     return 0;
 }
 
 
+		//http://www.nxp.com/assets/documents/data/en/supporting-information/Inter-Integrated-Circuit-Training.pdf
+void hardware_init(void) {
+
+  /* enable clock for PORTs */
+  CLOCK_SYS_EnablePortClock(PORTA_IDX);
+  CLOCK_SYS_EnablePortClock(PORTC_IDX);
+  CLOCK_SYS_EnablePortClock(PORTD_IDX);
+
+  configure_i2c_pins(1U);
+  /* Init board clock */
+  BOARD_ClockInit();
+  dbg_uart_init();
+}
