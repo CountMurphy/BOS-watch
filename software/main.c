8c0d39c5bbb97a220b91541fcf1f5bf01740332a
cgugas <spartan.1086@gmail.com>
2017-05-27 15:41:00 -0500

    got i2c working

--- software/main.c
+++ software/main.c
@@ -6,6 +6,7 @@
 #include "fsl_i2c_master_driver.h"
 #include "fsl_port_hal.h"
 #include "fsl_clock_manager.h"
+#include "fsl_gpio_driver.h"
 #include "fsl_debug_console.h"
 
 #define DATA_LENGTH             64
@@ -19,10 +20,15 @@ uint8_t rxBuff[DATA_LENGTH] = {0};
 i2c_device_t compass=
 {
 	.address=0x32>>1,
-	.baudRate_kbps=400
+	.baudRate_kbps=100
 };
 
 i2c_master_state_t compass_state;
+
+// static const gpio_output_pin_user_config_t wiggle={
+//     .pinName=GPIO_MAKE_PIN(GPIOC_IDX,9),
+//     .config.outputLogic=1
+// };
 
 
 int main (void)
@@ -30,29 +36,29 @@ int main (void)
     // Initialize i2c master
     hardware_init();
     OSA_Init();
-
+/*GPIO_DRV_OutputPinInit(&wiggle);
+while(1)
+{
+    GPIO_DRV_WritePinOutput(wiggle.pinName,1);
+    GPIO_DRV_WritePinOutput(wiggle.pinName,0);
+}*/
     I2C_DRV_MasterInit(BOARD_I2C_INSTANCE, &compass_state);
 
 	// Master sends 1 bytes CMD and data to slave
-    uint8_t cmd=0x50;
+    txBuff[0]=0x50;
 
     // OSA_TimeDelay(500);
 
     i2c_status_t retVal;
-    retVal= I2C_DRV_MasterSendDataBlocking(BOARD_I2C_INSTANCE, &compass, &cmd, 1,NULL , 0, 1000);
+    retVal= I2C_DRV_MasterSendDataBlocking(BOARD_I2C_INSTANCE, &compass, NULL, 0, txBuff, 1, 1000);
 
     // Delay to wait slave received data
     OSA_TimeDelay(1);
 
 
     // Master receives count byte data from slave
-    cmd=0x33;
-    I2C_DRV_MasterReceiveDataBlocking(BOARD_I2C_INSTANCE, &compass, &cmd, 0, rxBuff, 6, 1000);
+    I2C_DRV_MasterReceiveDataBlocking(BOARD_I2C_INSTANCE, &compass, NULL, 0, rxBuff, 6, 1000);
 
-    if(rxBuff)
-    {
-      //break here
-    }
 
     return 0;
 }
@@ -71,4 +77,6 @@ void hardware_init(void)
     PORT_HAL_SetMuxMode(PORTC,8u,kPortMuxAlt2);
     /* Affects PORTB_PCR1 register */
     PORT_HAL_SetMuxMode(PORTC,9u,kPortMuxAlt2);
+    //PORT_HAL_SetMuxMode(PORTC,9u,kPortMuxAsGpio);
+
 }
