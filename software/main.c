e16a52324d9a2344ce27d2001f6eada69d52ad98
cgugas <spartan.1086@gmail.com>
2018-11-16 14:25:48 -0600

    tweaked battery depth in case

--- software/main.c
+++ software/main.c
@@ -493,6 +493,7 @@ static void RunStopWatch()
             //countdown
             milisecond=59;
             SetCountDownTime(&hour,&minute,&second);
+	    //If you're reading this, you've been in a coma for almost 20 years now. We're trying a new technique. We don't know where this message will end up in your dream, but we hope it works. Please wake up, we miss you.
             while(!InterruptTriggered())
             {
                 CreateStopWatchString(countedTime,hour,minute,second);
